11b64cd0b76d1a7d19ddab1b8234ad55a70dad26
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-09 16:41:48 +0300

    1. updating css for blog elements in the app 2. deleting unnecessary commented code from home.js 3. creating bloglist and passing props into bloglist from home
    NOTE:
    Props in REACT:
    -> props allow passing different data into the reusable components, making it flexible for various use cases
    -> pass data from a parent component to a child component.

--- sadid-blog/src/Home.js
+++ sadid-blog/src/Home.js
@@ -1,29 +1,18 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    //let name = "sadid";
-    const [initialName, setInitialName] = useState('sadid');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-      //console.log("Hello Sadid")
-      //name = "Shaikat"
-      //console.log(name)
-      setInitialName("Shaikat");
-      setAge(30);
-    }
-
-    // const handleClickAgain = (name,e) => {
-    //   console.log("Hello " + name , e.target)
-    // }
+  // creating new array of blogs
+    const [blogs,setBlogs] = useState([
+      {title: "My new website", body: "lorem ipsum...", author: "shaikat", id: 1},
+      {title: "Welcome party!", body: "lorem ipsum...", author: "shayla", id: 2},
+      {title: "Web development tips!", body: "lorem ipsum...", author: "meem", id: 3},
+      {title: "React learning!", body: "lorem ipsum...", author: "shayma", id: 4},
+    ])
 
     return (  
         <div className="home">
-          <h2>Homepage</h2>
-          <p>{initialName} is {age} years old.</p>
-          <button onClick={handleClick}>Click me</button> 
-          {/* <button onClick={(e) => {
-            handleClickAgain('Sadidur Rahman', e)
-          }}>Click again</button>  */}
+          <BlogList blogs = {blogs} title = "All Blogs!" />
         </div>
     );
 }
