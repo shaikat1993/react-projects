8f332594193d23bffcb9246b04cd21c88fb52e88
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-12 13:28:46 +0300

    1. creating a json file inside data folder for removing the static data that we provide using an array 2. removing the static data which we provide using an array in home.js file
    Note: to use the json server we have to use:
    
    npx json-server --watch data/db.json --port 8000
    
    -> to start the json server
    -> locate where we created the json file
    -> instruct which port to run the json server since we use 3000 for the react project

--- sadid-blog/src/Home.js
+++ sadid-blog/src/Home.js
@@ -3,12 +3,12 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   // creating new array of blogs
-    const [blogs,setBlogs] = useState([
-      {title: "My new website", body: "lorem ipsum...", author: "shaikat", id: 1},
-      {title: "Welcome party!", body: "lorem ipsum...", author: "shayla", id: 2},
-      {title: "Web development tips!", body: "lorem ipsum...", author: "meem", id: 3},
-      {title: "React learning!", body: "lorem ipsum...", author: "shayma", id: 4},
-    ])
+    // const [blogs,setBlogs] = useState([
+    //   {title: "My new website", body: "lorem ipsum...", author: "shaikat", id: 1},
+    //   {title: "Welcome party!", body: "lorem ipsum...", author: "shayla", id: 2},
+    //   {title: "Web development tips!", body: "lorem ipsum...", author: "meem", id: 3},
+    //   {title: "React learning!", body: "lorem ipsum...", author: "shayma", id: 4},
+    // ])
 
     const [name, setName] = useState('shaikat');
 
