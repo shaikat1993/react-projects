169cf68bb4295a44c4c4bc9676962d7d5e211e76
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-12 13:13:39 +0300

    here we learnt how we can use useEffect dependencies

--- sadid-blog/src/Home.js
+++ sadid-blog/src/Home.js
@@ -10,10 +10,12 @@ const Home = () => {
       {title: "React learning!", body: "lorem ipsum...", author: "shayma", id: 4},
     ])
 
+    const [name, setName] = useState('shaikat');
+
     useEffect(() => {
       console.log("Use effect ran");
-      console.log(blogs);
-    })
+      console.log(name);
+    }, [name])
     const handleDelete =(id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
       setBlogs(newBlogs)
@@ -23,6 +25,8 @@ const Home = () => {
         <div className="home">
           <BlogList blogs = {blogs} title = "All Blogs!" handleDelete={handleDelete}/>
           {/* <BlogList blogs = {blogs.filter((blog) => blog.author === 'shaikat')} title = "Sadid's Blogs!!" /> */}
+          <button onClick={ () => setName('sadidur') }>Change name</button>
+          <p> {name} </p>
         </div>
     );
 }
